d25597279292a88feb387cdf1f161b5adc6eda5e
Anthony Beckett <antneyb10@gmail.com>
2024-09-01 19:19:10 +0100

    Added some query filtering to get all stocks query

--- api/Interfaces/IStockRepository.cs
+++ api/Interfaces/IStockRepository.cs
@@ -1,11 +1,12 @@
 using api.Dto.Stock;
+using api.Helpers;
 using api.Models;
 
 namespace api.Interfaces
 {
     public interface IStockRepository
     {
-        Task<List<Stock>> GetAllAsync();
+        Task<List<Stock>> GetAllAsync(QueryObject query);
 
         Task<Stock?> GetByIdAsync(int id);
 

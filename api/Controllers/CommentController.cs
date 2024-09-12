a0b99cda407c33081976223c1e88787a32eafc7f
Anthony Beckett <antneyb10@gmail.com>
2024-09-12 19:39:31 +0100

    Added API endpoint to populate stock and seed data from the api if it doesn't already exist

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -16,11 +16,18 @@ namespace api.Controllers
         private readonly ICommentRepository _commentRepository;
         private readonly IStockRepository _stockRepository;
         private readonly UserManager<AppUser> _userManager;
+        private readonly IFMPService _fmpService;
 
-        public CommentController(ICommentRepository commentRepository, IStockRepository stockRepository, UserManager<AppUser> userManager)
+        public CommentController(
+            ICommentRepository commentRepository, 
+            IStockRepository stockRepository, 
+            UserManager<AppUser> userManager,
+            IFMPService fmpService
+        )
         {
             _stockRepository = stockRepository;
             _userManager = userManager;
+            _fmpService = fmpService;
             _commentRepository = commentRepository;
         }
 
@@ -54,21 +61,29 @@ namespace api.Controllers
             return Ok(comment.ToCommentDto());
         }
 
-        [HttpPost("{stockId:int}")]
-        public async Task<IActionResult> Create([FromRoute] int stockId, CreateCommentDto commentDto)
+        [HttpPost("{symbol:alpha}")]
+        public async Task<IActionResult> Create([FromRoute] string symbol, CreateCommentDto commentDto)
         {
             if (! ModelState.IsValid) {
                 return BadRequest(ModelState);
             }
 
-            if (!await _stockRepository.StockExists(stockId)) {
-                return BadRequest("Stock does not exist");
+            var stock = await _stockRepository.GetBySymbolAsync(symbol);
+
+            if (stock == null) {
+                stock = await _fmpService.FindStockBySymbolAsync(symbol);
+
+                if (stock == null) {
+                    return BadRequest("This stock does not exist");
+                } else {
+                    await _stockRepository.CreateAsync(stock);
+                }
             }
 
             var username = User.GetUsername();
             var appUser = await _userManager.FindByNameAsync(username);
 
-            var commentModel = commentDto.ToCommentFromCreate(stockId);
+            var commentModel = commentDto.ToCommentFromCreate(stock.Id);
             commentModel.AppUserId = appUser.Id;
 
             await _commentRepository.CreateAsync(commentModel);
--- api/Controllers/PortfolioController.cs
+++ api/Controllers/PortfolioController.cs
@@ -14,16 +14,19 @@ namespace api.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly IStockRepository _stockRepository;
         private readonly IPortfolioRepository _portfolioRepository;
+        private readonly IFMPService _fmpService;
 
         public PortfolioController(
             UserManager<AppUser> userManager,
             IStockRepository stockRepository,
-            IPortfolioRepository portfolioRepository
+            IPortfolioRepository portfolioRepository,
+            IFMPService fmpService
         )
         {
             _userManager = userManager;
             _stockRepository = stockRepository;
             _portfolioRepository = portfolioRepository;
+            _fmpService = fmpService;
         }
 
         [HttpGet]
@@ -44,6 +47,16 @@ namespace api.Controllers
             var username = User.GetUsername();
             var appUser = await _userManager.FindByNameAsync(username);
             var stock = await _stockRepository.GetBySymbolAsync(symbol);
+
+            if (stock == null) {
+                stock = await _fmpService.FindStockBySymbolAsync(symbol);
+
+                if (stock == null) {
+                    return BadRequest("This stock does not exist");
+                } else {
+                    await _stockRepository.CreateAsync(stock);
+                }
+            }
 
             if (stock == null) {
                 return BadRequest("Stock not found");

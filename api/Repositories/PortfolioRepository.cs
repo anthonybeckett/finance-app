1c3f45a624afee2a5a6ff3e4588f87b93ca31c83
Anthony Beckett <antneyb10@gmail.com>
2024-09-12 12:26:23 +0100

    Created the add portfolio endpoint for the API

--- api/Repositories/PortfolioRepository.cs
+++ api/Repositories/PortfolioRepository.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using api.Data;
 using api.Interfaces;
 using api.Models;
@@ -16,6 +12,14 @@ namespace api.Repositories
         public PortfolioRepository(ApplicationDBContext context)
         {
             _context = context;
+        }
+
+        public async Task<Portfolio> CreateAsync(Portfolio portfolio)
+        {
+            await _context.Portfolios.AddAsync(portfolio);
+            await _context.SaveChangesAsync();
+
+            return portfolio;
         }
 
         public async Task<List<Stock>> GetUserPortfolio(AppUser user)

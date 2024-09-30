2218c89927bc54720f39d96ec68f5e33194729cf
Anthony Beckett <antneyb10@gmail.com>
2024-09-30 11:31:26 +0100

    Added auth context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,17 @@ import { Outlet } from "react-router";
 import Navbar from "./Components/Navbar";
 import "react-toastify/dist/ReactToastify.css"
 import { ToastContainer } from "react-toastify";
+import { UserProvider } from "./Context/useAuth";
 
 function App() {
     return (
-        <div>
-			<Navbar />
-			<Outlet />
-            <ToastContainer />
-		</div>
+        <>
+            <UserProvider>
+                <Navbar />
+                <Outlet />
+                <ToastContainer />
+            </UserProvider>
+		</>
     );
 }
 
